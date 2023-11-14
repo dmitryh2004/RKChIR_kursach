b0fa752980a531821956aae7baaf7642b2f1b49c
dmitry_h <113709033+dmitryh2004@users.noreply.github.com>
2023-11-14 12:03:35 +0300

    14.11.23 12:03

---
+++ js/main_init.js
@@ -0,0 +1,8 @@
+function main_init()
+{
+    main_toogle_blocks_init();
+    toogle_block('sec1_content', false, 0);
+    toogle_block('sec2_content', false, 1);
+    toogle_block('sec3_content', false, 2);
+    toogle_block('sec4_content', false, 3);
+}
